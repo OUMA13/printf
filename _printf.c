46979ef5ffa64ea573db72232087e67b8d52b9e1
OUMA13 <omaimaelkhanchoufi9@gmail.com>
2023-10-16 11:12:00 -0700

    first commit

--- _printf.c
+++ _printf.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
-/**
- * _printf - prints anything
- * @format: the format string
- *
- * Return: nmber of bytes printed
- */
+void print_buffer(char buffer[], int *buff_ind);
 
+/**
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
